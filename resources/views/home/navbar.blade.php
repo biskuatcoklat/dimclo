f40636ec41c403cd71cc959b7d7084b387af69d5
Wahyu Aprilliandhika <wahyuandhika910@gmail.com>
2023-06-16 00:26:21 +0700

    update

---
+++ resources/views/home/navbar.blade.php
@@ -0,0 +1,45 @@
+<div class="md:flex md:flex-row md:justify-between text-center">
+        <div class="flex flex-row justify-center">
+          <a href="index.html"
+            ><img src="images/logo3.png" alt="Logo"
+          /></a>
+        </div>
+        <div class="mt-2">
+          <a
+            href="index.html"
+            class="text-black-600 active-nav-link font-bold text-red-600 hover:text-red-600 p-1.5 md:p-4"
+            >Home</a
+          >
+          <a
+            href="product.html"
+            class="text-black-600 hover:text-red-600 p-1.5 md:p-4"
+            >Product</a
+          >
+          <a href="#" class="text-black-600 hover:text-red-600 p-1.5 md:p-4"
+            >Cart</a
+          >
+          <a href="#" class="text-black-600 hover:text-red-600 p-1.5 md:p-4"
+            >Order</a
+          >
+          <!-- <a
+            href="#"
+            class="text-gray-600 hover:text-green-600 p-1.5 md:p-4 hidden md:inline-block"
+            >Order</a
+          > -->
+          <a href="#" class="text-black-600 hover:text-red-600 p-1.5 md:p-4"
+            >Contact</a
+          >
+          <a
+            href="#"
+            class="bg-red-600 text-gray-50 hover:bg-red-700 p-2 rounded-md"
+          >
+            Login
+          </a>
+          <!-- <a
+            href="#"
+            class="bg-blue-600 text-gray-50 hover:bg-blue-700 p-2 rounded-full"
+          >
+            Register
+          </a> -->
+        </div>
+      </div>
